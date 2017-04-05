339f43d6f795fa435be821a5bab08950549fa0ff
中村　麟太郎 <NRintaro@lime.local>
2017-04-05 16:47:22 +0900

    単語の登録を可能に

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -3,9 +3,20 @@ class WordsController < ApplicationController
   end
 
   def new
+    # @word に空のModelオブジェクトを作成し、インスタンス変数@wordに設定
+    # @wordはViewでも使える
+    @word = Word.new
   end
 
   def create
+    @word =  Word.new( word_params )
+    if @word.save
+      # @wordはword_path(@word)に自動変換される
+      redirect_to @word, notice: "単語が登録されました。"
+    else
+      # ValidationエラーなどでDBに保存できない場合 new.html.erbを再表示
+      render 'new'
+    end
   end
 
   def destroy
@@ -15,5 +26,12 @@ class WordsController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  # セキュリティのため、permitメソッドで許可したパラメータ名しか取得できない
+  def word_params
+    params.require( :word ).permit( :word, :reading, :meaning, :example )
   end
 end
