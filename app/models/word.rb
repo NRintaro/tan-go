b05f8295d05c8f586fc4b05052e56b4e99e95f83
中村　麟太郎 <NRintaro@lime.local>
2017-04-05 16:46:19 +0900

    スペルミスを修正

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,6 +1,6 @@
 class Word < ApplicationRecord
   validates :word, length: { maximum: 50 }, presence: true
   validates :reading, length: { maximum: 100 }
-  validates :meanig, length: { maximum: 1000 }, presence: true
+  validates :meaning, length: { maximum: 1000 }, presence: true
   validates :example, length: { maximum: 2000 }
 end
