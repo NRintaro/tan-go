f9d498a7884dc6656ecbacd6291625f4b239cf18
中村　麟太郎 <NRintaro@lime.local>
2017-04-05 15:21:52 +0900

    wordモデルの定義を修正

--- db/migrate/20170404090725_create_words.rb
+++ db/migrate/20170404090725_create_words.rb
@@ -1,9 +1,9 @@
 class CreateWords < ActiveRecord::Migration[5.0]
   def change
     create_table :words do |t|
-      t.string :word
+      t.string :word,    null: false
       t.string :reading
-      t.string :meaning
+      t.string :meaning, null: false
       t.text :example
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,9 @@
 ActiveRecord::Schema.define(version: 20170404090725) do
 
   create_table "words", force: :cascade do |t|
-    t.string   "word"
+    t.string   "word",       null: false
     t.string   "reading"
-    t.string   "meaning"
+    t.string   "meaning",    null: false
     t.text     "example"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
